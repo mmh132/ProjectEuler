c22d7fbc2ee07fb3120075ae36969c915ee58bd2
mmh132 <mhorwitz@eastsideprep.org>
2023-07-30 09:30:26 -0700

    brute force working for 338

--- work/P338.py
+++ work/P338.py
@@ -1,17 +1,18 @@
-#working theory, cuts make it 
-
+from math import isqrt
 def bfF(x,y):
-    rv = 0
-    things = set()
-    for i in range(1, max(x, y)):
+    out = set()
+    for i in range(1, max(x,y)):
         if x%i == 0 and y%(i+1) == 0:
-            rv += 1 if x//i > 1 or y//(i+1) > 1 else 0
-            things.add(tuple(sorted((x*(i+1)//i, y*i//(i+1)))))
+            new = [(x//i)*(i+1), (y//(i+1))*i]
+            out.add(tuple(sorted(new)))
         if y%i == 0 and x%(i+1) == 0:
-            rv += 1 if y//i > 1 or x//(i+1) > 1 else 0
-            things.add(tuple(sorted((y*(i+1)//i, x*i//(i+1)))))
-    print(x,y,things)
-    return len(things) - 1 if (x,y) in things else 0
+            new = [(y//i)*(i+1), (x//(i+1))*i]
+            out.add(tuple(sorted(new)))
+    #print(x,y,out)
+    rv = len(out)
+    if tuple(sorted([x,y])) in out:
+        rv -= 1
+    return rv
 
 def bfG(n):
     rv = 0
@@ -20,4 +21,4 @@ def bfG(n):
             rv += bfF(x,y)
     return rv
 
-print(bfG(10))
+print(bfG(1000))
