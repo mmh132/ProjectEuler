5c199f63d899d74ee02dc19ac861f1516d120780
mmh132 <mhorwitz@eastsideprep.org>
2024-01-03 10:01:56 -0800

    Update P642.py

--- work/P642.py
+++ work/P642.py
@@ -1,5 +1,6 @@
 from math import isqrt
-
+import sys
+sys.setrecursionlimit(100000)
 
 def FIinc(n):
     i, la = 1,0
@@ -16,7 +17,8 @@ def FIdec(n):
         i = la + 1
 
 def f(n):
-    lim = int(n**1/3)
+    lim = int(n**(2/5))
+    
     vals = dict()
     primes = []
     for i in FIinc(n):
@@ -27,13 +29,15 @@ def f(n):
         for v in FIdec(n):
             if v < p*p: break
             vals[v] -= p*(vals[v//p] - vals[p-1])
-    print(vals)
 
     cache = dict()
     def smooth(n, i):
-        if (n, i) in cache: return cache[(n, i)]
-        if i < 0: return 0
-        if n < 2: return n
+        if (n, i) in cache: 
+            return cache[(n, i)]
+        if n == 0:
+            return 0
+        if i == -1:
+            return 1
         rv = smooth(n, i-1) + smooth(n//primes[i], i)
         if n < lim:
             cache[(n, i)] = rv
@@ -43,8 +47,10 @@ def f(n):
     for i in range(len(primes)):
         p_i = primes[i]
         rv += p_i*smooth(n//p_i, i)
-    for i in range(1, isqrt(n)+1):
+        #print(smooth(n//p_i, i), p_i, n//p_i)
+    for i in range(1, isqrt(n)):
+        #print(vals[n//i] - vals[n//(i+1)])
         rv += (vals[n//i] - vals[n//(i+1)])*i
     return rv
 
-print(f(10))
+print(f(201820182018))
